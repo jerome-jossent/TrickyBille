f93db46a981b47d0b6d80cadf99d8d2dac9107c4
jerome.jossent@gmail.com <jerome.jossent@gmail.com>
2020-06-26 13:13:22 +0200

    test planche

---
+++ Assets/_Scripts/applyTorque.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class applyTorque : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float torque;
+    float turn;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        //turn = Input.GetAxis("Horizontal");
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            turn = 1;
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        rb.AddTorque(transform.forward * torque * turn);
+    }
+}
