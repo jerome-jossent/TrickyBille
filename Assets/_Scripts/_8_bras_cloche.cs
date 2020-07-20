231f2a9e1db11478cb414d7072195b0e0f79ddc6
Jerome Jossent <67312941+jerome-jossent@users.noreply.github.com>
2020-07-20 23:02:26 +0200

    Bras cloche : debut

---
+++ Assets/_Scripts/_8_bras_cloche.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _8_bras_cloche : MonoBehaviour
+{
+    [SerializeField] _controller controller;
+   // [SerializeField] GameObject GO_withRigidBody;
+   // Rigidbody rigidbody;
+    public float rotationSpeed;
+    public float xAngle, yAngle, zAngle;
+
+
+    private void Start()
+    {
+       // rigidbody = GO_withRigidBody.GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (controller.gamepad == null) return;
+        
+        if(controller.gamepad.xButton.wasPressedThisFrame)
+        {
+           // transform.RotateAround(target.transform.position, Vector3.up, 20 * Time.deltaTime);
+
+            transform.Rotate(xAngle, yAngle, zAngle, Space.Self);
+            //transform.Rotate(xAngle, yAngle, zAngle, Space.World);
+            //transform.Rotate(Vector3.forward, rotationSpeed);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+}
